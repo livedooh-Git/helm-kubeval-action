39ed4fd6713ac9ebd5bd98c2fed6dab3199411b2
Monika <81293197+mon-pan@users.noreply.github.com>
2021-06-15 21:35:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,29 +22,14 @@ run_kubeval() {
     rm -rf helm-output;
 }
 
-# Parse helm repos located in the $CONFIG_FILE file / Ignore commented lines (#)
-while read REPO_CONFIG
-do
-    case "$REPO_CONFIG" in \#*) continue ;; esac
-    REPO=$(echo $REPO_CONFIG | cut -d '=' -f1);
-    URL=$(echo $REPO_CONFIG | cut -d '=' -f2);
-    helm repo add $REPO $URL;
-done < $CONFIG_FILE
-
 # For all charts (i.e for every directory) in the directory
 for CHART in "$CHARTS_PATH"/*/; do
     echo "Validating $CHART Helm Chart...";
     cd "$CURRENT_DIR/$CHART";
     helm dependency build;
 
-    if [ -d "ci" ]; then
-        for VALUES_FILE in ci/*-values.yaml; do
-            echo "Validating $CHART Helm Chart using $VALUES_FILE values file...";
-            run_kubeval "$(pwd)" "$VALUES_FILE"
-        done
-    else
-        run_kubeval "$(pwd)" "/dev/null"
-    fi
-    echo "Cleanup $(pwd)/charts directory after we are done running Kubeval"
-    rm -rf $(pwd)/charts/
+    for VALUES_FILE in values-*.yaml; do
+        echo "Validating $CHART Helm Chart using $VALUES_FILE values file...";
+        run_kubeval "$(pwd)" "$VALUES_FILE"
+    done
 done
