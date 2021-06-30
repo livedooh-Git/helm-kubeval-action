9643dec4f967928247fb5a001f8623efbf8f95c4
Monika <81293197+mon-pan@users.noreply.github.com>
2021-06-30 15:13:06 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,8 @@ run_kubeval() {
     # Validate all generated manifest against Kubernetes json schema
     cd "$1"
     VALUES_FILE="$2"
-    mkdir helm-output;
-    echo "Print this"
-    helm template --values "$VALUES_FILE" --output-dir helm-output . > /dev/null 2>&1;
+    mkdir helm-output
+    helm template --values "$VALUES_FILE" --output-dir helm-output . > /dev/null 2>&1
     find helm-output -type f -exec \
         /kubeval/kubeval \
             "-o=$OUTPUT" \
@@ -19,8 +18,8 @@ run_kubeval() {
             "--kubernetes-version=$KUBERNETES_VERSION" \
             "--openshift=$OPENSHIFT" \
             "--ignore-missing-schemas=$IGNORE_MISSING_SCHEMAS" \
-        {} +;
-    rm -rf helm-output;
+        {} +
+    rm -rf helm-output
 }
 
 # For all charts (i.e for every directory) in the directory
