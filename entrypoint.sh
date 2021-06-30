417f44dd6bcbb4ffb7e45bc1c4ff9f6760269ff5
Monika <81293197+mon-pan@users.noreply.github.com>
2021-06-30 14:49:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ run_kubeval() {
             "--openshift=$OPENSHIFT" \
             "--ignore-missing-schemas=$IGNORE_MISSING_SCHEMAS" \
         {} +;
-    rm -rf helm-output &> dev/null;
+    rm -rf helm-output &> /dev/null;
 }
 
 # For all charts (i.e for every directory) in the directory
