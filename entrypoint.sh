99c38f8d171e0c0cdf907dcd0c48e66859962cfd
Monika <81293197+mon-pan@users.noreply.github.com>
2021-10-25 15:11:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ for CHART in "$CHARTS_PATH"/*/; do
     
     for VALUES_FILE in values*.yaml; do
         run_kubeval "$(pwd)" "$VALUES_FILE" | grep -Ev "PASS|wrote|Set" | awk 'NF';
-        if (run_kubeval "$(pwd)" "$VALUES_FILE" | grep -Ev "ERR|invalid")
+        if (run_kubeval "$(pwd)" "$VALUES_FILE" | grep -eq "ERR|invalid")
             then
                 echo "Errors found, setting exit status to 1."
                 exit 1
