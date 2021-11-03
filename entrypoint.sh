a28f69b041ceabd368606139641aa295107ba507
Monika <81293197+mon-pan@users.noreply.github.com>
2021-11-03 22:48:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,8 @@ for CHART in "$CHARTS_PATH"/*/; do
     
     for VALUES_FILE in values*.yaml; do
         RESULT=$(run_kubeval "$(pwd)" "$VALUES_FILE");
+        echo $RESULT;
+        echo $RESULT | grep -Ev 'PASS|wrote|Set';
         echo $RESULT | grep -Ev 'PASS|wrote|Set' | awk 'NF';
         
         errorMatchCount=$(echo $RESULT | grep -E '^ERR|^Error|invalid' | wc -c);
